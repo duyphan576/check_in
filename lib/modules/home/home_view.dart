4284dc26e86e49ccbcdbcf446d87f977d7d811da
duyphan576 <74853096+duyphan576@users.noreply.github.com>
2023-07-07 10:18:56 +0700

    Update home_view.dart

--- lib/modules/home/home_view.dart
+++ lib/modules/home/home_view.dart
@@ -3,42 +3,43 @@ import 'package:flutter/material.dart';
 import 'widgets/student_data.dart';
 
 class HomeView extends StatelessWidget {
-  const HomeView({required this.students});
+  const HomeView({super.key, required this.students});
   final Students students;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        StudentName(
-                          studentName: students.fullname.toString(),
-                        ),
-                      ],
-                    ),
-                    StudentPicture(
-                        picAddress: 'assets/images/student_profile.jpeg',
-                        onPress: () {}),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [],
-                )
-              ],
-            ),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          StudentName(
+                            studentName: students.fullname.toString(),
+                          ),
+                        ],
+                      ),
+                      StudentPicture(
+                          picAddress: 'assets/images/student_profile.jpeg',
+                          onPress: () {}),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [],
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
