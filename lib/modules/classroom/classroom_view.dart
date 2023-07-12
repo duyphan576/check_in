472ea89c7c7eca78a080de868c38649f6ab8347f
duyphan576 <74853096+duyphan576@users.noreply.github.com>
2023-07-12 15:54:09 +0700

    update

---
+++ lib/modules/classroom/classroom_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ClassroomView extends StatelessWidget {
+  const ClassroomView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
