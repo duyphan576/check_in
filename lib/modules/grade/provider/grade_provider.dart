0c879f464e60cae6779c070112372dbc76287ae6
jukute2000 <136227245+LTBao2000@users.noreply.github.com>
2023-07-20 13:24:51 +0700

    up

--- lib/modules/grade/provider/grade_provider.dart
+++ lib/modules/grade/provider/grade_provider.dart
@@ -9,7 +9,9 @@ class GradeProvider extends GetConnect {
 
   final HttpProvider http;
   Future<BaseResponse?> grade(GradeModel gradeModel, url, token) async {
-    return await http.doGetWithToken(url, token).then((response) {
+    Map<String, dynamic> submit = Map<String, dynamic>();
+    submit.addAll(gradeModel.toMap());
+    return await http.doGetWithToken(url, token, submit).then((response) {
       return BaseResponse(
           statusCode: response.statusCode,
           statusText: response.statusMessage,
