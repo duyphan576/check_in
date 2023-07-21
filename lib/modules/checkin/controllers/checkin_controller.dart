d1a212d368a9e422f759b7dc9958766ea7ca2ecf
duyphan576 <74853096+duyphan576@users.noreply.github.com>
2023-07-21 15:31:06 +0700

    update

--- lib/modules/checkin/controllers/checkin_controller.dart
+++ lib/modules/checkin/controllers/checkin_controller.dart
@@ -1,12 +1,9 @@
 import 'package:check_in/core/cache_manager.dart';
-import 'package:check_in/modules/checkin/models/checkin_model.dart';
 import 'package:check_in/modules/checkin/repository/checkin_repository.dart';
-import 'package:check_in/routes/app_pages.dart';
 import 'package:check_in/services/authenticationService.dart';
-import 'package:check_in/services/domain_service.dart';
 import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/status/http_status.dart';
 import 'package:network_info_plus/network_info_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class CheckinController extends GetxController with CacheManager {
   final CheckinRepository checkinRepository;
@@ -26,12 +23,38 @@ class CheckinController extends GetxController with CacheManager {
   initData() async {
     final info = NetworkInfo();
 
-    final wifiName = await info.getWifiName(); // "FooNetwork"
-    final wifiBSSID = await info.getWifiBSSID(); // 11:22:33:44:55:66
-    final wifiIP = await info.getWifiIP(); // 192.168.1.43
+    bool isGranted = await requestWifiInfoPermisson();
+    if (isGranted) {
+      String? wifiName = await info.getWifiName(); // FooNetwork
+      String? wifiBSSID = await info.getWifiBSSID(); // 11:22:33:44:55:66
+      String? wifiIP = await info.getWifiIP();
+      print(wifiName);
+      print(wifiBSSID);
+      print(wifiIP);
+    }
+  }
 
-    print(wifiName);
-    print(wifiBSSID);
-    print(wifiIP);
+  Future<bool> requestWifiInfoPermisson() async {
+    // ignore: avoid_print
+    print('Checking Android permissions');
+    PermissionStatus status = await Permission.location.status;
+    // Blocked?
+    if (status.isDenied || status.isRestricted) {
+      // Ask the user to unblock
+      if (await Permission.location.request().isGranted) {
+        // Either the permission was already granted before or the user just granted it.
+        // ignore: avoid_print
+        print('Location permission granted');
+        return true;
+      } else {
+        // ignore: avoid_print
+        print('Location permission not granted');
+        return false;
+      }
+    } else {
+      // ignore: avoid_print
+      print('Permission already granted (previous execution?)');
+      return true;
+    }
   }
 }
