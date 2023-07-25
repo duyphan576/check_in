8b780ef576fc7b7b1a301ecb0a5a20c251e642f3
duyphan576 <74853096+duyphan576@users.noreply.github.com>
2023-07-25 14:28:11 +0700

    Update checkin_view.dart

--- lib/modules/checkin/views/checkin_view.dart
+++ lib/modules/checkin/views/checkin_view.dart
@@ -64,21 +64,9 @@ class CheckinView extends GetView<CheckinController> {
                                     onDetect: (capture) {
                                       final List<Barcode> barcodes =
                                           capture.barcodes;
-                                      final Uint8List? image = capture.image;
                                       for (final barcode in barcodes) {
                                         debugPrint(
                                             'Barcode found! ${barcode.rawValue}');
-                                      }
-                                      if (image != null) {
-                                        showDialog(
-                                          context: context,
-                                          builder: (context) =>
-                                              Image(image: MemoryImage(image)),
-                                        );
-                                        Future.delayed(
-                                            const Duration(seconds: 5), () {
-                                          Navigator.pop(context);
-                                        });
                                       }
                                     },
                                   ),
